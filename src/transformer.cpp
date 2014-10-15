ea3e64ec0fa620c8e4482e7bb2ede9194d8b43db
sumeetfefar <sumeet.fefar@gmail.com>
2014-10-16 00:16:37 +0530

    Surface Sky Done

---
+++ src/transformer.cpp
@@ -0,0 +1,89 @@
+#include "gl_framework.hpp"
+#include "robot.hpp"
+
+#include "load_texture.hpp"
+#include "draw_shapes.hpp"
+#include <GL/glu.h>
+#include <GL/gl.h>
+#include <math.h>
+#include <unistd.h>
+
+GLuint torso_tex;
+
+void init_textures(void){
+	torso_tex = loadTexture("texture/paris.bmp");
+}
+
+int main(int argc, char** argv){
+	//! The pointer to the GLFW window
+	GLFWwindow* window;
+
+	//! Setting up the GLFW Error callback
+	glfwSetErrorCallback(csX75::error_callback);
+
+	//! Initialize GLFW
+	if (!glfwInit())
+	return -1;
+
+	//! Create a windowed mode window and its OpenGL context
+	window = glfwCreateWindow(800, 800, "CS475 Assignment 2.1 | 10D070048 | 10D070001", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	//! Make the window's context current 
+	glfwMakeContextCurrent(window);
+
+	//Initialize GLEW
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		//Problem: glewInit failed, something is seriously wrong.
+		std::cerr<<"GLEW Init Failed : %s"<<std::endl;
+	}
+
+	//Print and see what context got enabled
+	std::cout<<"Vendor: "<<glGetString (GL_VENDOR)<<std::endl;
+	std::cout<<"Renderer: "<<glGetString (GL_RENDERER)<<std::endl;
+	std::cout<<"Version: "<<glGetString (GL_VERSION)<<std::endl;
+	std::cout<<"GLSL Version: "<<glGetString (GL_SHADING_LANGUAGE_VERSION)<<std::endl;
+
+	//Keyboard Callback
+	glfwSetKeyCallback(window, key_callback);
+	//Framebuffer resize callback
+	glfwSetFramebufferSizeCallback(window, csX75::framebuffer_size_callback);
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	//Initialize GL state
+	csX75::initGL();
+	init_structures();
+	
+	init_textures();
+	// Loop until the user closes the window
+	
+	while (glfwWindowShouldClose(window) == 0)
+	{
+
+		// Render here
+		renderGL();
+
+		// Swap front and back buffers
+		glfwSwapBuffers(window);
+		
+		if ((state == sHUMANOID) || (state == sVEHICLE)){
+			// Poll for and process events
+			glfwPollEvents();
+		} else {
+			usleep(TF_DELAY_uS);
+			//~ right_hip_rot.set(20, 50, 82);
+		}
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
